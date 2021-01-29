17bb12fd0078f655782d4c8d058ca3197cdb22a3
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-01-29 15:48:28 +0100

    Fetch all template URLs

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,18 @@ import InputFields from './InputFields';
 import PreviewMeme from './PreviewMeme';
 import Overlay from './Overlay';
 
+async function getPreviewTemplates() {
+  const response = await fetch('https://api.memegen.link/templates/');
+  const data = await response.json();
+  return data.map(
+    (item) => item.blank.split('.png')[0] + '.jpg?width=150&height=150'
+  );
+}
+
 export default function App() {
+  const previewMeme = getPreviewTemplates();
+  console.log(previewMeme);
+
   return (
     <>
       <h1>Meme Generator</h1>
--- src/components/PreviewMeme.js
+++ src/components/PreviewMeme.js
@@ -1,10 +1,3 @@
 export default function Images() {
-  return (
-    <div className="previewMeme">
-      <img
-        src="https://api.memegen.link/images/buzz/memes/memes_everywhere.png"
-        alt="text"
-      />
-    </div>
-  );
+  return <div className="previewMeme"></div>;
 }
